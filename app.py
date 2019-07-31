f4066ac71b0705215a2e3f0f98799e5eac34e150
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:37:04 -0400

    omersuvks dick

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,
 render_template
 app = Flask(__name__)
 
@@ -8,5 +8,5 @@ def hello_world():
 "index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
 
